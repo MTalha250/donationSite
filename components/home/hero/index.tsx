45bcd9359c1c79cc69d65845bc85ce7b37a3a1db
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-08-07 16:47:26 +0500

    update

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -1,24 +1,32 @@
 "use client";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Hero = () => {
+  const { scrollY } = useScroll();
+  const scale = useTransform(scrollY, [0, 300], [1, 1.1]);
+
   const [scroll, setScroll] = useState("0");
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    const handleScroll = () => {
       if (window.scrollY > 150) {
         setScroll("100");
       } else {
         setScroll("0");
       }
-    });
-  });
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="text-[#18412E] h-[80vh] fixed w-full top-0">
-      <img
+      <motion.img
         src="https://d25oniaj7o2jcw.cloudfront.net/full-illustration-desktop-20220802@2x.jpg"
         alt=""
         className="w-full h-full object-cover absolute top-0 left-0"
+        style={{ scale }}
       />
       <div className="bg-fixed flex flex-col justify-center items-center space-y-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <p className="text-xl font-semibold">Leading crowdfunding platform</p>
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   );
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 0) {
+      if (window.scrollY > 100) {
         setScroll("rounded-none top-0 scale-100");
       } else {
         setScroll("text-lg rounded-full top-5 scale-[0.8]");
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <div
       className={
-        "z-50 w-full left-0 bg-white fixed shadow-lg transition duration-300 py-6 px-8 md:px-16 " +
+        "z-50 w-full left-0 bg-white fixed shadow-lg transition duration-300 ease-in-out py-6 px-8 md:px-16 " +
         scroll
       }
     >
